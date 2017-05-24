705978af3e66d53627f4367f2ec331ef30a91854
a034871vz <a034871vz@gmail.com>
2017-05-24 19:29:59 +0300

    123

--- Lab5 B.py
+++ Lab5 B.py
@@ -1,22 +1,27 @@
-fname = "mbox.txt"
-file = open(fname)
 adr = all_mail = Conf = all_Conf = spam = 0
 a = set()
-l = [line.split() for line in file if line.startswith("From ") and line.find("@") > 0 and len(line.split()) > 2]
-file.closed
-file = open(fname)
-k = [line.split()for line in file if line.startswith("X-DSPAM-Confidence:")]
+s1 = "From"
+s2 = "X-DSPAM-Confidence:"
+fname = "mbox.txt"
+def search(fname, str):
+    fname = fname
+    file = open(fname)
+    search = [line.split() for line in file if line.startswith(str)]
+    file.closed
+    return search
+l = search(fname, s1)
+k = search(fname, s2)
 for i in range(len(k)):
-    adr = (l[i][1])
+    adr = l[i][1]
     Conf = k[i][1]
     all_Conf += float(Conf)
     all_mail += 1
     spam = all_Conf/all_mail
     print("по адресу:", adr, "значение X-DSPAM-Confidence равно:", Conf)
-    if float(Conf) >= 0.96:
+    if float(Conf) >= spam:
         a.add(adr)
     else:
         continue
 for i in a:
     print("Забанить - ", i)
-print("Всего писем:", all_mail, "Среднее значение X-DSPAM-Confidence:", spam, "Забанить за спам нужно:", len(a))
+print("Всего писем:", all_mail, "Среднее значение X-DSPAM-Confidence:", round(spam, 4), "Забанить за спам нужно:", len(a))
--- Lab5OOP.py
+++ Lab5OOP.py
@@ -1,29 +1,29 @@
 class Mail:
     adr = all_mail = Conf = all_Conf = spam = 0
     a = set()
-    def search1(self,fname):
-        file = open(fname)
-        l = [line.split() for line in file if line.startswith("From ") and line.find("@") > 0 and len(line.split()) > 2]
+    def Search(self, fname, str):
+        self.fname = fname
+        file = open(self.fname)
+        search = [line.split() for line in file if line.startswith(str)]
         file.closed
-        return  l
-    def search2(self, fname):
-        file = open(fname)
-        k = [line.split() for line in file if line.startswith("X-DSPAM-Confidence:")]
-        file.closed
-        return k
+        return search
 mail = Mail()
-
-for i in range(len(mail.search1('mbox.txt'))):
-    mail.adr = (mail.search1('mbox.txt')[i][1])
-    mail.Conf = mail.search2('mbox.txt')[i][1]
+s1 = "From"
+s2 = "X-DSPAM-Confidence:"
+fname = "mbox.txt"
+mail.l = mail.Search(fname, s1)
+mail.k = mail.Search(fname, s2)
+for i in range(len(mail.k)):
+    mail.adr = mail.l[i][1]
+    mail.Conf = mail.k[i][1]
     mail.all_Conf += float(mail.Conf)
     mail.all_mail += 1
     mail.spam = mail.all_Conf/mail.all_mail
     print("по адресу:", mail.adr, "значение X-DSPAM-Confidence равно:", mail.Conf)
-    if float(mail.Conf) >= 0.96:
+    if float(mail.Conf) >= mail.spam:
         mail.a.add(mail.adr)
     else:
         continue
 for i in mail.a:
     print("Забанить - ", i)
-print("Всего писем:", mail.all_mail, "Среднее значение X-DSPAM-Confidence:", mail.spam, "Забанить за спам нужно:", len(mail.a))
+print("Всего писем:", mail.all_mail, "Среднее значение X-DSPAM-Confidence:", round(mail.spam, 4), "Забанить за спам нужно:", len(mail.a))
